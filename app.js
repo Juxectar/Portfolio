c275f27e9b440d904490149b0887fe6adae8fac4
Juxectar <oliverhiim1@gmail.com>
2019-09-06 20:42:23 +0200

    snow effect oversight on div1

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const backsnow = document.getElementById("backsnow");
 const nosnow = document.getElementById("nosnow");
 
 const profilebackground = document.getElementById("profilebackground");
+const div1background = document.getElementById("div1background");
 
 function snow() {
     fullsnow.addEventListener("click", function () {
         document.getElementById("body").style.backgroundSize = "unset";
         profilebackground.style.zIndex = "-1";
+        div1background.style.zIndex = "-1";
     });
     backsnow.addEventListener("click", function () {
         document.getElementById("body").style.backgroundSize = "unset";
         profilebackground.style.zIndex = "0";
+        div1background.style.zIndex = "0";
     });
     nosnow.addEventListener("click", function () {
         document.getElementById("body").style.backgroundSize = "0";
