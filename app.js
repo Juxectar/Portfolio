f4ad9862f19f811d72a5faa49b8f9845b34635cb
Juxectar <oliverhiim1@gmail.com>
2019-09-18 20:43:31 +0200

    add calculator and some minor changes

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const nosnow = document.getElementById("nosnow");
 
 const profilebackground = document.getElementById("profilebackground");
 const div1background = document.getElementById("div1background");
+const div2background = document.getElementById("div2background");
 
 function snow() {
     fullsnow.addEventListener("click", function () {
         document.getElementById("body").style.backgroundSize = "unset";
         profilebackground.style.zIndex = "-1";
         div1background.style.zIndex = "-1";
+        div2background.style.zIndex = "-1";
     });
+    
     backsnow.addEventListener("click", function () {
         document.getElementById("body").style.backgroundSize = "unset";
         profilebackground.style.zIndex = "0";
         div1background.style.zIndex = "0";
+        div2background.style.zIndex = "0";
     });
+
     nosnow.addEventListener("click", function () {
         document.getElementById("body").style.backgroundSize = "0";
     });
