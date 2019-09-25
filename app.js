1dab83d5628042c1ee6e599a7a627841d7a5bede
Juxectar <oliverhiim1@gmail.com>
2019-09-25 15:25:26 +0200

    javascript simplification + some code cleanup

--- app.js
+++ app.js
@@ -2,31 +2,30 @@ const fullsnow = document.getElementById("fullsnow");
 const backsnow = document.getElementById("backsnow");
 const nosnow = document.getElementById("nosnow");
 
-const profilebackground = document.getElementById("profilebackground");
-const div1background = document.getElementById("div1background");
-const div2background = document.getElementById("div2background");
+const allBackgrounds = document.getElementsByClassName("projectbackground");
+
+
 
 function snow() {
+
     fullsnow.addEventListener("click", function () {
-        document.getElementById("body").style.backgroundSize = "unset";
-        profilebackground.style.zIndex = "-1";
-        div1background.style.zIndex = "-1";
-        div2background.style.zIndex = "-1";
+        document.querySelector("body").style.backgroundSize = "unset";
+        for (var i = 0; i < allBackgrounds.length; i++) {
+            allBackgrounds[i].style.zIndex = "-1";
+        };
+
     });
-    
+
     backsnow.addEventListener("click", function () {
-        document.getElementById("body").style.backgroundSize = "unset";
-        profilebackground.style.zIndex = "0";
-        div1background.style.zIndex = "0";
-        div2background.style.zIndex = "0";
-    });
+        document.querySelector("body").style.backgroundSize = "unset";
+        for (var i = 0; i < allBackgrounds.length; i++) {
+            allBackgrounds[i].style.zIndex = "0";
+        };
 
+    });
     nosnow.addEventListener("click", function () {
-        document.getElementById("body").style.backgroundSize = "0";
+        document.querySelector("body").style.backgroundSize = "0";
     });
-};
-
-
-
+}
 
 snow();
